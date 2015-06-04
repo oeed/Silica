517af587f3afa943e1f28578bc21ae6008ee6066
Oliver Cooper <oliver.cooper@me.com>
2015-06-04 15:41:57 +0930

    Merged the two class APIs

--- class.lua
+++ class.lua
@@ -1,25 +1,29 @@
 local class = {}
+local classes = {}
+
+local creating
+local USE_GLOBALS = true
 
 -- @static
 function class:new( ... )
 	local newRaw = {}
 
-	newRaw.super = self.__extends --:new(...) -- not sure here, do we want super to be a class or instance? i'll do some testing
+	newRaw.super = self._extends --:new( ... ) -- not sure here, do we want super to be a class or instance? i'll do some testing
 	newRaw.class = self
 
-	setmetatable(newRaw, { __index = self })
+	setmetatable( newRaw, { __index = self } )
 
-	-- these are to prevent infinite loops in getters and setters (so, for example, doing self.speed = 10 in setSpeed doesn't cause setSpeed to be called a million times)
+	-- these are to prevent infinite loops in getters and setters ( so, for example, doing self.speed = 10 in setSpeed doesn't cause setSpeed to be called a million times )
 	local lockedSetters = {}
 	local lockedGetters = {}
 
 	local newProxy = {} -- the proxy. 'self' always needs to be this, NOT newRaw
-	setmetatable(newProxy, {
-		__index = function(t, k)
+	setmetatable( newProxy, {
+		__index = function( t, k )
 			-- this basically allows a global filter or notification on all get
-			if not lockedGetters[k] and newRaw.get and type(newRaw.get) == 'function' then
+			if not lockedGetters[k] and newRaw.get and type( newRaw.get ) == 'function' then
 				lockedGetters[k] = true
-				local use, value = newRaw.get(newProxy, k)
+				local use, value = newRaw.get( newProxy, k )
 				lockedGetters[k] = nil
 				if use then
 					return value
@@ -27,12 +31,12 @@ function class:new( ... )
 			end
 
 			-- basically, if the get'Key' function is set, return it's value
-			-- names are capitalised, so .name becomes getName; This might cause a few issues regarding collisions, but really, how many (good) coders are using .name and .Name
+			-- names are capitalised, so .name becomes getName; This might cause a few issues regarding collisions, but really, how many ( good ) coders are using .name and .Name
 			-- non-function properties can't use it, because, well, it's just futile really
-			local getFunc = 'get' .. k:sub(1, 1):upper() .. k:sub(2, -1)
-			if not lockedGetters[k] and type(newRaw[k]) ~= 'function' and newRaw[getFunc] and type(newRaw[getFunc]) == 'function' then
+			local getFunc = 'get' .. k:sub( 1, 1 ):upper() .. k:sub( 2, -1 )
+			if not lockedGetters[k] and type( newRaw[k] ) ~= 'function' and newRaw[getFunc] and type( newRaw[getFunc] ) == 'function' then
 				lockedGetters[k] = true
-				local value = newRaw[getFunc](newProxy)
+				local value = newRaw[getFunc]( newProxy )
 				lockedGetters[k] = nil
 				return value
 			else
@@ -40,11 +44,11 @@ function class:new( ... )
 			end
 		end,
 
-		__newindex = function (t,k,v)
+		__newindex = function ( t,k,v )
 			-- this basically allows a global filter or notification on all sets
-			if not lockedSetters[k] and type(newRaw.set) and type(newRaw.set) == 'function' then
+			if not lockedSetters[k] and type( newRaw.set ) and type( newRaw.set ) == 'function' then
 				lockedSetters[k] = true
-				local use, value = newRaw.set(newProxy, k, v)
+				local use, value = newRaw.set( newProxy, k, v )
 				lockedSetters[k] = nil
 				if use then
 					newRaw[k] = value
@@ -53,86 +57,132 @@ function class:new( ... )
 			end
 
 			-- if the filter wasn't applied, if the set'Key' function is set, call it
-			local setFunc = 'set' .. k:sub(1, 1):upper() .. k:sub(2, -1)
-			if not lockedSetters[k] and type(newRaw[k]) ~= 'function' and newRaw[setFunc] and type(newRaw[setFunc]) == 'function' then
+			local setFunc = 'set' .. k:sub( 1, 1 ):upper() .. k:sub( 2, -1 )
+			if not lockedSetters[k] and type( newRaw[k] ) ~= 'function' and newRaw[setFunc] and type( newRaw[setFunc] ) == 'function' then
 				lockedSetters[k] = true
-				newRaw[setFunc](newProxy, v)
+				newRaw[setFunc]( newProxy, v )
 				lockedSetters[k] = nil
 			else
 				newRaw[k] = v -- use the passed value if not using the setter
 			end
 		end
-	})
+	} )
 
 	-- use the setters with all the starting values
-	for k, v in pairs(self) do
-		if type(self[k]) ~= 'function' then
+	for k, v in pairs( self ) do
+		if type( self[k] ) ~= 'function' then
 			newProxy[k] = v
 		end
 	end
 
     -- once the class has been created, pass the arguments to the init function for handling
-    if newProxy.init and type(newProxy.init) == 'function' then
+    if newProxy.init and type( newProxy.init ) == 'function' then
     	newProxy:init( ... )
     end
 
 	return newProxy
 end
 
--- constructs an actual class (NOT instance)
+-- constructs an actual class ( NOT instance )
 -- @static
-function class:construct(_, name) -- for some reason self is passed twice, not sure why
+function class:construct( _, name ) -- for some reason self is passed twice, not sure why
     local _class = {}
     _class.name = name
-    _class.__extends = nil
-    _class.__metatable = {
-        __call = function(self, ...)
-            return self:new(...)
-        end,
-        __index = self
-    }
-    setmetatable(_class, _class.__metatable)
 
-    -- not sure if _G is the right thing to use here
-    _G[name] = _class
+    local mt = { __index = self }
+    _class.mt = mt
 
-    -- _class can't directly be returned here, but nor can a single function (as :extends and {} are the possible options afterwards)
-    local response = {}
-    setmetatable(response, {
-        __index = _class,
-        __call = function(_response, properties)
-            _class:properties(properties)
-        end
-    })
-    return response
+    function mt:__call( ... )
+        return self:new( ... )
+    end
+
+ 	function mt:__tostring()
+    	return self.name
+    end
+
+    setmetatable( _class, mt )
+
+    classes[name] = _class
+    getfenv( 2 )[name] = _class
+    creating = _class
+
+    return function( properties )
+    	creating = nil
+	_class:properties( properties )
+	return _class
+    end
 end
 
 -- @instance
-function class:properties(properties)
-    for k, v in pairs(properties) do
+function class:properties( properties )
+    for k, v in pairs( properties ) do
     	self[k] = v
     end
 end
 
 -- @instance
-function class:extends(superName)
-    local _class = getmetatable(self).__index -- get the actual class from 'response'
-    if not getfenv()[superName] then
+function class:typeOf( _class )
+	if type( self ) ~= "table" then
+		return false
+	elseif self.class then
+		return self.class:typeOf( _class )
+	elseif self == _class then
+		return true
+	elseif self._extends then
+		return self._extends:typeOf( _class )
+	end
+	return false
+end
+
+-- @instance
+function class:type()
+	local _type = type( self )
+	pcall( function()
+		_type = getmetatable( self ).__type or _type
+	end )
+	return _type
+end
+
+-- @instance
+function class:can( method )
+	return type( self[method] ) == "function"
+end
+
+setmetatable( class, {
+    __call = function( ... ) 
+    	return class:construct( ... )
+    end
+} )
+
+local function extends( superName )
+    if not classes[superName] then
         -- TODO: add an autoloading here to load the class if it's not found
-        error('Super class for `' .. _class.name .. '` was not found: ' .. superName)
+        error( 'Super class for `' .. creating.name .. '` was not found: ' .. superName )
     end
 
-    _class.__extends = getfenv()[superName]
-    _class.__metatable.__index = _class.__extends
-    setmetatable(_class, _class.__metatable)
-    return function(properties)
-        _class:properties(properties)
+    creating._extends = classes[superName]
+	for k, v in pairs( classes[superName].mt ) do
+		creating.mt[k] = v
+	end
+    creating.mt.__index = classes[superName]
+
+    setmetatable( creating, creating.mt )
+
+    return function( properties )
+    	local _class = creating
+        creating:properties( properties )
+        creating = nil
+        return _class
     end
 end
 
-setmetatable(class, {
-    __call = function(...) return class:construct(...) end
-})
+if USE_GLOBALS then
+	getfenv().class = class
+	getfenv().extends = extends
+else
+	class.extends = extends
+	return class
+end
 
 
 --- tests
@@ -141,24 +191,32 @@ class "Object" {
     x = 5;
 }
 
-function Object:init(properties) -- we can decided how to structure the arguments to new later
+function Object:init( properties ) -- we can decided how to structure the arguments to new later
 	properties = properties or {}
-	for k, v in pairs(properties) do
+	for k, v in pairs( properties ) do
 		self[k] = v
 	end
 end
 
-class "Button" : extends 'Object' {
+class "Button" extends 'Object' {
     y = 1;
 }
 
-print(Button.x) -- 5
-print(Button.y) -- 1
+function Button:init( properties )
+	self.super.init( self, properties )
+end
 
-local myButton = Button({
+print( Button.x ) -- 5
+print( Button.y ) -- 1
+
+local myObject = Object( {
+	text = 'hi';
+} )
+
+local myButton = Button( {
 	x = 4;
 	text = 'hi';
-})
-print(myButton.x) -- 4
-print(myButton.y) -- 1
-print(myButton.text) -- hi
+} )
+print( myButton.x ) -- 4
+print( myButton.y ) -- 1
+print( myButton.text ) -- hi
