e480c24119796c0985dd3cc37b5031c794d841e0
Oliver Cooper <oliver.cooper@me.com>
2015-11-21 17:48:03 +1030

    Started Mask

---
+++ src/classes/Graphics/Mask.lua
@@ -0,0 +1,15 @@
+
+class "Mask" {
+    
+    x = Number;
+    y = Number;
+
+    pixels = Table;
+
+}
+
+function Mask:initialise( Number x, Number y, Table pixels )
+    self.x = x    
+    self.y = y    
+    self.pixels = pixels    
+end
