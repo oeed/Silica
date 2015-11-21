c80c6d7dc84b7b4eee317e402316989fdfa78604
Oliver Cooper <oliver.cooper@me.com>
2015-11-21 17:38:24 +1030

    Added place Canvas function placeholders

---
+++ src/classes/Graphics/Canvas.lua
@@ -0,0 +1,35 @@
+
+class "Canvas" {
+    
+    buffer = Table( {} );
+
+}
+
+--[[
+    @desc Fills an area in the given mask with the given colour
+]]
+function Canvas:fill( Mask mask, Graphics.colours colour )
+end
+
+--[[
+    @desc Draws an outline around the given mask
+]]
+function Canvas:outline( Mask mask, Graphics.colours colour, Number( 1 ) thickness )
+end
+
+--[[
+    @desc Draws the canvas to another canvas at the specified location
+    @return Any returnedValue
+]]
+
+--[[
+    @desc Description
+]]
+function Canvas:drawTo( Number x, Number y, Canvas destinationCanvas, Mask.allowsNil mask )
+end
+
+--[[
+    @desc Draws an image to the canvas, scaling the image if needed
+]]
+function Canvas:image( Number x, Number y, Image image, Number( image.width ) width, Number( image.height ) height )
+end
