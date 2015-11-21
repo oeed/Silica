962b3b4b632781ef11046030aef706c6eca503ec
Oliver Cooper <oliver.cooper@me.com>
2015-11-21 15:01:22 +1030

    Created test application for graphics testing

---
+++ testapplication/classes/TestApplication.lua
@@ -0,0 +1,22 @@
+
+class "TestApplication" extends "Application" {
+	name = "Test";
+	interfaceName = "test";
+}
+
+function TestApplication:initialise()
+	self:super()
+	self:event( CharacterEvent, self.onChar )
+end
+
+--[[
+	@instance
+	@desc React to a character being fired
+	@param [Event] event -- description
+	@return [boolean] stopPropagation
+]]
+function TestApplication:onChar( CharacterEvent event, Event.phases phase )
+	if not self:hasFocus() and event.character == '\\' then
+		os.reboot()
+	end
+end
