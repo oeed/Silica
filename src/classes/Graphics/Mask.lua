5db8f8402fef9ad100e05deb3ec43f5a04962b4e
Oliver Cooper <oliver.cooper@me.com>
2015-11-22 11:44:45 +1030

    Required mask to define width and height

--- src/classes/Graphics/Mask.lua
+++ src/classes/Graphics/Mask.lua
@@ -3,13 +3,17 @@ class "Mask" {
     
     x = Number;
     y = Number;
+    width = Number;
+    height = Number;
 
     pixels = Table;
 
 }
 
-function Mask:initialise( Number x, Number y, Table( {} ) pixels )
-    self.x = x    
-    self.y = y    
+function Mask:initialise( Number x, Number y, Number width, Number height, Table( {} ) pixels )
+    self.x = x
+    self.y = y
+    self.width = width
+    self.height = height
     self.pixels = pixels    
 end
