767f8e3cf54b1717ba78ddb4a5acc8933f506810
Oliver Cooper <oliver.cooper@me.com>
2015-11-22 11:52:39 +1030

    Created RectangleMask

---
+++ src/classes/Graphics/RectangleMask.lua
@@ -0,0 +1,13 @@
+
+class "RectangleMask" extends "Mask" {
+    
+}
+
+function RectangleMask:initialise( Number x, Number y, Number width, Number height )
+    local pixels = {}
+    for i = 1, width * height do
+        pixels[i] = true
+    end
+
+    self:super( x, y, width, height, pixels )
+end
