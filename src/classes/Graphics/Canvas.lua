84d6969090c41b7071b0e5760cf4866092693d01
Oliver Cooper <oliver.cooper@me.com>
2015-11-22 12:49:09 +1030

    Added Canvas:fill and :drawTo methods

--- src/classes/Graphics/Canvas.lua
+++ src/classes/Graphics/Canvas.lua
@@ -6,6 +6,8 @@ class "Canvas" {
     width = Number;
     height = Number;
     owner = View.allowsNil;
+    mask = Mask.allowsNil; -- the canvas' rectangle
+    contentMask = Mask.allowsNil; -- the canvas' content
 
     pixels = Table( {} );
 
@@ -20,15 +22,34 @@ function Canvas:initialise( Number x, Number y, Number width, Number height, Vie
 end
 
 --[[
-    @desc Fills an area in the given mask with the given colour
+    @desc Creates a mask which covers the entire canvas
 ]]
-function Canvas:fill( Mask mask, Graphics.colours colour )
+function Canvas.mask:get()
+    return RectangleMask( 1, 1, self.width, self.height )
 end
 
 --[[
-    @desc Draws an outline around the given mask
+    @desc Fills an area in the given mask with the given colour, defaulting to the entire canvas
 ]]
-function Canvas:outline( Mask mask, Graphics.colours colour, Number( 1 ) thickness )
+function Canvas:fill( Graphics.colours colour, Mask( self.mask ) mask )
+    local pixels, width, height = self.pixels, self.width, self.height
+    local maskX, maskY, maskWidth, maskHeight = mask.x, mask.y, mask.width, mask.height
+    for index, isFilled in pairs( mask.pixels ) do
+        if isFilled then
+            local x = (index - 1) % maskWidth + maskX
+            local y = math.floor( ( index - 1) / maskWidth ) + maskY
+            if x >= 1 and x <= width and y >= 1 and y <= height then
+                pixels[( y - 1 ) * width + x] = colour
+            end
+        end
+    end
+end
+
+--[[
+    @desc Draws an outline around the given mask, defaulting to the canvas' content mask
+]]
+function Canvas:outline( Graphics.colours colour, Number( 1 ) thickness, Mask( self.contentMask ) mask )
+
 end
 
 --[[
@@ -40,6 +61,18 @@ end
     @desc Draws the canvas to another canvas
 ]]
 function Canvas:drawTo( Number x, Number y, Canvas destinationCanvas, Mask.allowsNil mask )
+    local width, height = self.width, self.height
+    local destinationX, destinationY, destinationWidth, destinationHeight, destinationPixels = destinationCanvas.x, destinationCanvas.y, destinationCanvas.width, destinationCanvas.height, destinationCanvas.pixels
+    local TRANSPARENT = Graphics.colours.TRANSPARENT
+    for index, colour in pairs( self.pixels ) do
+        if colour and colour ~= TRANSPARENT then
+            local _x = (index - 1) % width + x
+            local _y = math.floor( ( index - 1) / width ) + y
+            if _x >= 1 and _x <= width and _y >= 1 and _y <= height then
+                destinationPixels[( _y - 1 ) * width + _x] = colour
+            end
+        end
+    end
 end
 
 --[[
@@ -51,5 +84,5 @@ end
 --[[
     @desc Draws a shadow mask to the parent's canvas
 ]]
-function Canvas:drawShadow( Number x, Number y, Number shadowSize, Mask shadowMask, Graphics.colours shadowColour )
+function Canvas:drawShadow( Number x, Number y, Number shadowSize, Graphics.colours shadowColour, Mask shadowMask )
 end
