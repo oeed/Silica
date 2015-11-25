aa37314c3f37d8f2b29addca80f929db357b8729
Oliver Cooper <oliver.cooper@me.com>
2015-11-25 20:19:44 +1030

    Converted SeparatorMenuItem to new graphics system

--- src/classes/Views/SeparatorMenuItem.lua
+++ src/classes/Views/SeparatorMenuItem.lua
@@ -1,15 +1,8 @@
 
 class "SeparatorMenuItem" extends "MenuItem" {
-	text = false;
 
 	height = Number( 3 );
 	width = Number( 51 );
-
-	textColour = Graphics.colours.LIGHT_GREY;
-
-    pressedTextColour = Graphics.colours.WHITE;
-
-    disabledTextColour = Graphics.colours.LIGHT_GREY;
 
 }
 
@@ -19,22 +12,15 @@ class "SeparatorMenuItem" extends "MenuItem" {
 	@param [table] properties -- the properties for the view
 ]]
 function SeparatorMenuItem:initialise( ... )
-	self.super:super( ... )
+	self.super:super( ... ) -- by pass the normal menuitem's event connecting, we don't need to get any events
 end
 
-function SeparatorMenuItem:initialiseCanvas()
-	self.super:super()
-    self.backgroundObject = self.canvas:insert( Separator( 5, 2, self.width - 8, 1 ) )
+function SeparatorMenuItem:onDraw()
+    local width, height, theme, canvas, isPressed = self.width, self.height, self.theme
+    canvas:fill( theme:value( "fillColour" ) )
+    local leftMargin = theme:value( "leftMargin" )
+    local separatorX, separatorY, separatorWidth = 1 + leftMargin, 1 + theme:value( "topMargin" ), width - leftMargin - theme:value( "rightMargin" )
+    self.canvas:fill( theme:value( "separatorColour" ), theme:value( "isDashed" ) and SeparatorMask( separatorX, separatorY, separatorWidth, 1 ) or RectangleMask( separatorX, separatorY, separatorWidth, 1 ) )
 end
 
-function SeparatorMenuItem.isPressed:set( isPressed )
-    self.isPressed = false
-end
-
-function SeparatorMenuItem:updateWidth( width )
-	self.backgroundObject.width = width - 8
-end
-
-function SeparatorMenuItem:updateHeight( height )
-	self.backgroundObject.height = 1
 end
