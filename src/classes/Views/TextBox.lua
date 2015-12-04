38d62109f184f96a0eb047983fc10d6bfaa514d3
Oliver Cooper <oliver.cooper@me.com>
2015-12-04 21:09:58 +1030

    Adding selection and cursor clipping to TextBox

--- src/classes/Views/TextBox.lua
+++ src/classes/Views/TextBox.lua
@@ -78,14 +78,16 @@ function TextBox:onDraw()
 	end
 
     local scroll = self.scroll
+    local outlineThickness = theme:value( "outlineThickness" )
     if isFocused then
     	local fontHeight = font.height
+		local contentMask = RectangleMask( leftMargin - 1, 1 + outlineThickness, width - leftMargin - rightMargin + 4, height - 2 * outlineThickness )
     	if self.selectionVisible then
     		local selectionWidth = self.selectionWidth
     		if selectionWidth > 0 then
 	    		local selectionLeftMargin, selectionRightMargin, selectionTopMargin, selectionBottomMargin = theme:value( "selectionLeftMargin" ), theme:value( "selectionRightMargin" ), theme:value( "selectionTopMargin" ), theme:value( "selectionBottomMargin" )
 	    		local selectionMask = RoundedRectangleMask( leftMargin + 1 + self.selectionX - scroll - selectionLeftMargin, math.floor( fontHeight / 2 ) - selectionTopMargin, selectionWidth + selectionLeftMargin + selectionRightMargin, fontHeight + selectionTopMargin + selectionBottomMargin, theme:value( "selectionRadius" ) )
-	    		canvas:fill( theme:value( "selectionColour" ), selectionMask )
+	    		canvas:fill( theme:value( "selectionColour" ), selectionMask:intersect( contentMask ) )
 	    	end
     	end
 
@@ -93,13 +95,12 @@ function TextBox:onDraw()
     	local cursorColour = self.cursorColour
     	if cursorColour ~= fillColour then
 	    	local cursorMask = RectangleMask( leftMargin + 1 + self.cursorX - scroll, math.floor( fontHeight / 2 ), 1, fontHeight + 1 )
-    		canvas:fill( cursorColour, cursorMask )
+    		canvas:fill( cursorColour, cursorMask:intersect( contentMask ) )
     	end
     end
 
     local textMask = roundedRectangle:intersect( TextMask( leftMargin + 1 - scroll, topMargin + 1, font:getWidth( text ), height - topMargin - bottomMargin, text, font ) )
     canvas:fill( theme:value( "textColour" ), textMask  )
-    local outlineThickness = theme:value( "outlineThickness" )
     canvas:fill( theme:value( "fadeOneColour" ), OutlineMask( leftMargin, topMargin, width - leftMargin - rightMargin + 2, height - topMargin - bottomMargin + 2 * outlineThickness ):intersect( textMask )  )
     canvas:fill( theme:value( "fadeTwoColour" ), OutlineMask( leftMargin - 1, topMargin - 1, width - leftMargin - rightMargin + 4, height - topMargin - bottomMargin + 4 * outlineThickness ):intersect( textMask )  )
     canvas:fill( theme:value( "fillColour" ), OutlineMask( 1 + outlineThickness, 1 + outlineThickness, width - 2 * outlineThickness, height - 2 * outlineThickness, leftMargin - 2 - outlineThickness, topMargin - 2 - outlineThickness, rightMargin - 2 - outlineThickness, bottomMargin - 2 - outlineThickness ):intersect( textMask ) )
