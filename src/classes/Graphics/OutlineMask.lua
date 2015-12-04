1db3fdec53892e3956720bf1be450d800b5eea7e
Oliver Cooper <oliver.cooper@me.com>
2015-12-04 20:55:44 +1030

    Added adjustable thickness to OutlineMask

--- src/classes/Graphics/OutlineMask.lua
+++ src/classes/Graphics/OutlineMask.lua
@@ -3,13 +3,13 @@ class "OutlineMask" extends "Mask" {
 
 }
 
-function OutlineMask:initialise( Number x, Number y, Number width, Number height )
+function OutlineMask:initialise( Number x, Number y, Number width, Number height, Number( 1 ) leftThickness, Number( leftThickness ) topThickness, Number( leftThickness ) rightThickness, Number( topThickness ) bottomThickness )
     local pixels = {}
     local i = 0
     for y = 1, height do
         for x = 1, width do
             i = i + 1
-            pixels[i] = ( y == 1 or y == height or x == 1 or x == width ) or nil
+            pixels[i] = ( y <= topThickness or y > height - bottomThickness or x <= leftThickness or x > width - rightThickness ) or nil
         end
     end
 
