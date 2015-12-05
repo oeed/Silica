c65d55ff8d41589c72829d3b00555790af72ee50
Oliver Cooper <oliver.cooper@me.com>
2015-12-05 18:02:21 +1030

    Removed redundant code from WindowContainer

--- src/classes/Views/Containers/WindowContainer.lua
+++ src/classes/Views/Containers/WindowContainer.lua
@@ -1,23 +1,4 @@
 
 class "WindowContainer" extends "Container" {
 
-	backgroundObject = false;
-
 }
-
-function WindowContainer:initialise( ... )
-	self:super( ... )
-end
-
-function WindowContainer:initialiseCanvas()
-	self.canvas = WindowCanvas( self.x, self.y, self.width, self.height )
-	self.backgroundObject = self.canvas:insert( Rectangle( 1, 1, self.width, self.height, Graphics.colours.WHITE ) )
-end
-
-function WindowContainer:updateHeight( height )
-	self.backgroundObject.height = height
-end
-
-function WindowContainer:updateWidth( width )
-	self.backgroundObject.width = width
-end
