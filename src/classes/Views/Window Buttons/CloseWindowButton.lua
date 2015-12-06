5346689adcce1460b36ad5de4439afdbac06ab7a
Oliver Cooper <oliver.cooper@me.com>
2015-12-06 23:00:04 +1030

    Removed :initialiseCanvas from WindowButton subclasses

--- src/classes/Views/Window Buttons/CloseWindowButton.lua
+++ src/classes/Views/Window Buttons/CloseWindowButton.lua
@@ -1,19 +1,6 @@
 
 class "CloseWindowButton" extends "WindowButton" {}
 
-function CloseWindowButton:initialiseCanvas()
-	self:super()
-
-	-- local symbolObject = Path( 4, 3, 3, 3 )
- --    symbolObject:lineTo( 3, 3 )
- --    symbolObject:moveTo( 3, 1 )
- --    symbolObject:lineTo( 1, 3 )
- --    symbolObject:close( false )
-
- --    self.theme:connect( symbolObject, "outlineColour", "symbolColour" )
- --    self.symbolObject = self.canvas:insert( symbolObject )
-end
-
 function CloseWindowButton:onMouseUp( Event event, Event.phases phase )    
     if self.window then
         self.window:close()
