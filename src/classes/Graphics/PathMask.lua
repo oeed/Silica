7307d6051933eaa27504eafb44b383717ec8b6f3
Oliver Cooper <oliver.cooper@me.com>
2015-12-08 10:56:56 +1030

    Added Path drawing mask

--- src/classes/Graphics/PathMask.lua
+++ src/classes/Graphics/PathMask.lua
@@ -1,13 +1,26 @@
 
-class "PathMask" {
+class "PathMask" extends "Mask" {
     
 }
 
 function PathMask:initialise( Number x, Number y, Path path )
+    local intersections = path:getIntersections()
     local pixels = {}
-    for i = 1, width * height do
-        pixels[i] = true
+    local width, height = path.width, path.height
+
+    for y = 1, height do
+        local yIntersections = intersections[y]
+        for i = 1, #yIntersections, 2 do
+            local x1, x2 = yIntersections[i], yIntersections[i + 1]
+            for x = math.floor( x1 + 0.5), math.floor( x2 + 0.5 ) do
+                pixels[(y - 1) * width + x] = true
+            end
+        end
     end
+
+    -- for i = 1, width * height do
+    --     pixels[i] = true
+    -- end
 
     self:super( x, y, width, height, pixels )
 end
