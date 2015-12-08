0a0e3288f41685b3d94cfc970503eb0afb41cbb9
Oliver Cooper <oliver.cooper@me.com>
2015-12-08 12:29:50 +1030

    Added scaleability to PathMask

--- src/classes/Graphics/PathMask.lua
+++ src/classes/Graphics/PathMask.lua
@@ -3,10 +3,10 @@ class "PathMask" extends "Mask" {
     
 }
 
-function PathMask:initialise( Number x, Number y, Path path )
-    local intersections = path:getIntersections()
+function PathMask:initialise( Number x, Number y, Path path, Number( path.width ) width,  Number( path.height ) height )
     local pixels = {}
-    local width, height = path.width, path.height
+    local scaleX, scaleY = width / path.width, height / path.height
+    local intersections = path:getIntersections( scaleX, scaleY )
 
     for y = 1, height do
         local yIntersections = intersections[y]
