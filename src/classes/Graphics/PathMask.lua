b40ae628cbaca5c0fd3b86a0989f749b874ec042
Oliver Cooper <oliver.cooper@me.com>
2015-12-12 18:41:10 +1030

    Fixed PathMask passing fill AND outline to self:super

--- src/classes/Graphics/PathMask.lua
+++ src/classes/Graphics/PathMask.lua
@@ -6,5 +6,6 @@ class "PathMask" extends "Mask" {
 function PathMask:initialise( Number x, Number y, Path path, Number( path.width ) width,  Number( path.height ) height )
     local pixels = {}
     local scaleX, scaleY = width / path.width, height / path.height
-    self:super( x, y, width, height, path:getFill( scaleX, scaleY ) )
+    local fill = path:getFill( scaleX, scaleY )
+    self:super( x, y, width, height, fill )
 end
