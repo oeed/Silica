ece228b9e942808b88e4c70b32ee63b2cef40461
Oliver Cooper <oliver.cooper@me.com>
2015-12-13 12:46:24 +1030

    Added action function argument checking

--- src/classes/class.lua
+++ src/classes/class.lua
@@ -560,6 +560,14 @@ function stripFunctionArguments( name, contents )
                         if tableItem[TYPETABLE_TYPE] then
                             ArgumentValueTypeParsingClassException( "Invalid setter arguments in class '" ..name .. "' on line " .. n .. ". The setter's argument cannot declare a ValueType, it is automatically inferred. (e.g. function View.isFocused:set( isFocused ) ). Read the 'Class System' wiki page if you're still stuck.", 0 )
                         end
+                    elseif functionName == "action" then
+                        if #argumentsTable ~= 1 then
+                            ArgumentValueTypeParsingClassException( "Invalid action arguments in class '" ..name .. "' on line " .. n .. ". Actions should can only have ONE argument. Read the 'Class System' wiki page if you're still stuck.", 0 )
+                        end
+                        local tableItem = argumentsTable[1]
+                        if tableItem[TYPETABLE_CLASS] ~= classes['ActionInterfaceEvent'] or tableItem[TYPETABLE_HAS_DEFAULT_VALUE] then
+                            ArgumentValueTypeParsingClassException( "Invalid action arguments in class '" ..name .. "' on line " .. n .. ". The action's argument must be an ActionInterfaceEvent without a default value (e.g. propertyName:action( ActionInterfaceEvent event) ). Read the 'Class System' wiki page if you're still stuck.", 0 )
+                        end
                     end
                 end
 
