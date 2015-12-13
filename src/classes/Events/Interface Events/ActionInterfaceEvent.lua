305ec627a1b66c119c73b71bf5cbe00dbe3ed874
Oliver Cooper <oliver.cooper@me.com>
2015-12-13 13:43:19 +1030

    Renamed ActionInterfaceEvent.sender to .view to avoid conflict with Event.sender

--- src/classes/Events/Interface Events/ActionInterfaceEvent.lua
+++ src/classes/Events/Interface Events/ActionInterfaceEvent.lua
@@ -5,7 +5,7 @@ class "ActionInterfaceEvent" extends "InterfaceEvent" {
         eventType = "interface_action";
     };
 
-	sender = View; -- the View whose action it is
+	view = View; -- the View whose action it is
     originalEvent = Event.allowsNil; -- the event the invoked this event (such as a mouse up, key click, etc.)
 
 }
@@ -13,10 +13,10 @@ class "ActionInterfaceEvent" extends "InterfaceEvent" {
 --[[
 	@constructor
 	@desc Creates a focus event from the arguments
-	@param [View] sender -- the view whose action it is
+	@param [View] view -- the View whose action it is
 ]]
-function ActionInterfaceEvent:initialise( View sender, Event.allowsNil originalEvent )
-	self.sender = sender
+function ActionInterfaceEvent:initialise( View view, Event.allowsNil originalEvent )
+	self.view = view
     self.originalEvent = originalEvent
 end
 
