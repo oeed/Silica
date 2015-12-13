551bb11a46c97c165a9d8c71259ad406d1a3747d
Oliver Cooper <oliver.cooper@me.com>
2015-12-13 14:28:38 +1030

    Removed test InterfaceLink

--- testapplication/classes/TestApplicationContainer.lua
+++ testapplication/classes/TestApplicationContainer.lua
@@ -1,8 +1,6 @@
 
 class "TestApplicationContainer" extends "ApplicationContainer" {
     
-    theButton = Label( "button" ).link;
-    
 }
 
 function TestApplicationContainer.theButton:action( ActionInterfaceEvent event )
