919482838bbe809abc05f0fa3d88a82c1d20934f
Oliver Cooper <oliver.cooper@me.com>
2015-12-14 19:05:29 +1030

    Remade LineMask so it actually works

--- src/classes/Graphics/LineMask.lua
+++ src/classes/Graphics/LineMask.lua
@@ -5,15 +5,21 @@ class "LineMask" extends "Mask" {
 
 function LineMask:initialise( Number x, Number y, Number width, Number height, Boolean( true ) isFromTopLeft )
     local pixels = {}
-    local i = 0
-    local slope = height / width
-    if height > width then
-        for y = 1, height do
-            pixels[(y - 1) * width + math.floor( y / slope + 0.5)] = true
+    local xDiff = width - 1
+    local yDiff = ( isFromTopLeft and height - 1 or -height + 1 )
+    if xDiff > math.abs(yDiff) then
+        local y = isFromTopLeft and 1 or height
+        local dy = yDiff / xDiff
+        for x = 1, width do
+            pixels[( math.floor( y + 0.5 ) - 1 ) * width + x ] = true
+            y = y + dy
         end
     else
-        for x = 1, width do
-            pixels[(math.floor(slope * x + 0.5) - 1) * width + x] = true
+        local x = isFromTopLeft and 1 or width
+        local dx = xDiff / yDiff
+        for y = 1, height do
+            pixels[(y - 1) * width + math.floor( x + 0.5 ) ] = true
+            x = x + dx
         end
     end
 
