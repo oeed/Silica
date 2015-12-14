477f8eb9552122c354e89b5b2a4c0552d039cf88
Oliver Cooper <oliver.cooper@me.com>
2015-12-14 11:19:10 +1030

    Fixed LineMask incorrectly plotting line

--- src/classes/Graphics/LineMask.lua
+++ src/classes/Graphics/LineMask.lua
@@ -6,17 +6,14 @@ class "LineMask" extends "Mask" {
 function LineMask:initialise( Number x, Number y, Number width, Number height, Boolean( true ) isFromTopLeft )
     local pixels = {}
     local i = 0
-    for y = 1, height do
-        if width > height then
-            local y = minY
-            for x = 1, width do
-                pixels[(y - 1) * width + floor( 1 + (x - 1) / width * height + 0.5 )] = true
-            end
-        else
-            local dx = ( height / width ) * inverseScaleX
-            for y = minY, maxY, inverseScaleY do
-                -- outline[floor( y * scaleY + 0.5 )][x1 + ( y - 1 ) / height * width] = true
-            end
+    local slope = height / width
+    if height > width then
+        for y = 1, height do
+            pixels[(y - 1) * width + math.floor( y / slope + 0.5)] = true
+        end
+    else
+        for x = 1, width do
+            pixels[(math.floor(slope * x + 0.5) - 1) * width + x] = true
         end
     end
 
