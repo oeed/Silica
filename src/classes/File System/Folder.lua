d8e96e0d22c40c0f7e725840e3943f99be4a6e53
Oliver Cooper <oliver.cooper@me.com>
2015-12-16 11:52:35 +1030

    Added functions to create empty files and folders within a folder

--- src/classes/File System/Folder.lua
+++ src/classes/File System/Folder.lua
@@ -82,6 +82,22 @@ function Folder:serialise( flatten, metadataProperties )
     return allItems
 end
 
+--[[
+    @desc Creates a folder within this folder
+    @return Folder
+]]
+function Folder:makeSubfolder( String path, Boolean( false ) overwrite )
+    return Folder.static:make( self.path .. tidy( path ), overwrite )
+end
+
+--[[
+    @desc Creates a file within this folder
+    @return Folder
+]]
+function Folder:makeSubfile( String path, Metadata.mimes mime, Boolean( false ) overwrite, String.allowsNil contents )
+    return File.static:make( self.path .. tidy( path ), mime, overwrite, contents )
+end
+
 function Folder:getItems( noFiles, noFolders )
     local items = {}
     local path = self.path
