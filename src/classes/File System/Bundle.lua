eef5490f7f317c1d2320b7977fdb9ab746b23148
Oliver Cooper <oliver.cooper@me.com>
2015-12-18 13:48:58 +1030

    Added Bundle config loading

--- src/classes/File System/Bundle.lua
+++ src/classes/File System/Bundle.lua
@@ -12,7 +12,11 @@ local function tidy( path )
     return path
 end
 
-class "Bundle" extends "Folder" {}
+class "Bundle" extends "Folder" {
+    
+    config = Table;
+
+}
 
 function Bundle.metatable:__call( path, ... )
     if fs.exists( path ) and fs.isDir( path ) and not fs.isReadOnly( path ) and fs.exists( tidy( path .. "/bundle.sconfig" ) ) then
@@ -21,4 +25,21 @@ function Bundle.metatable:__call( path, ... )
             return self.spawn( false, path, ... )
         end
     end
+end
+
+function Bundle.path:set( path )
+    path = tidy( path )
+    self:super( path )
+
+    local configFile = self:find( "bundle", Metadata.mimes.SCONFIG, true )
+    if not configFile then
+        error( "Bundle is corrupt (no bundle.sconfig or file mime is incorrect)." )
+    end
+
+    local config = configFile.serialisedContents
+    if not config then
+        error( "Bundle is corrupt (bundle.sconfig could not be parsed)." )
+    end
+
+    self.config = config
 end
